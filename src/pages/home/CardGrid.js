f54b04598c91827b310e147e849f50035998bfe7
Ryan Richetto <rrichetto15@gmail.com>
2022-02-02 01:23:43 -0800

    Add axios and implement error handling

--- src/pages/home/CardGrid.js
+++ src/pages/home/CardGrid.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
@@ -5,16 +6,27 @@ import Card from './Card';
 
 const CardGrid = () => {
   const [coinList, setCoinList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const fetchCoins = async () => {
-      const res = await fetch(
-        `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=26&tsym=USD&api_key=${process.env.REACT_APP_API_KEY}`
-      );
-      const json = await res.json();
+      setIsLoading(true);
+      setIsError(false);
 
-      // Only use coins that have DISPLAY object (which holds USD price)
-      setCoinList(json.Data.filter((coin) => coin.DISPLAY || false));
+      try {
+        const { data } = await axios.get(
+          `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=26&tsym=USD&api_key=${process.env.REACT_APP_API_KEY}`
+        );
+
+        // Only use coins that have DISPLAY object (which holds USD price)
+        setCoinList(data.Data.filter((coin) => coin.DISPLAY || false));
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        setIsError(true);
+        console.log(error);
+      }
     };
 
     fetchCoins();
@@ -24,9 +36,15 @@ const CardGrid = () => {
 
   return (
     <Wrap>
-      {coinList.map((coin) => (
-        <Card key={coin.CoinInfo.Id} coin={coin} />
-      ))}
+      {isLoading && <h2 className="loading">Loading Coins...</h2>}
+      {isError && (
+        <h2 className="loading">
+          Something went wrong, please try again soon.
+        </h2>
+      )}
+      {!isLoading &&
+        !isError &&
+        coinList.map((coin) => <Card key={coin.CoinInfo.Id} coin={coin} />)}
     </Wrap>
   );
 };
@@ -35,6 +53,10 @@ const Wrap = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(225px, 1fr));
   grid-gap: 4rem;
+
+  .loading {
+    text-align: center;
+  }
 `;
 
 export default CardGrid;
